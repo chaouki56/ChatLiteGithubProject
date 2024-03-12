300cd52b011bdadb7a9e895097440d74542944d6
chaouki <m.chaouki@digency.ma>
2024-03-12 23:56:35 +0000

    First commit

---
+++ src/main/java/com/example/chatlite/ChatLiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatLiteApplication.class, args);
+	}
+
+}
